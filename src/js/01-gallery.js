dd119e225f7f23bbf2448ee2be531443bcf15d70
NikNas1405 <103371039+NikNas1405@users.noreply.github.com>
2023-06-16 16:25:19 +0300

    1 TASK

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,35 @@
-// Add imports above this line
-import { galleryItems } from './gallery-items';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+// console.log(galleryItems);
+
+const galleryWrapper = document.querySelector('.gallery');
+function galleryItemsArray() {
+  return galleryItems
+    .map(({ preview, original, description }) => {
+      return ` <li class="gallery__item">
+    <a class="gallery__link" href="${original}">
+     <img
+      class="gallery__image"
+      src="${preview}"
+      alt="${description}"
+    />
+  </a>
+  </li>
+`;
+    })
+    .join('');
+}
+
+galleryWrapper.insertAdjacentHTML('beforeend', galleryItemsArray(galleryItems));
+
+let lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  //   captionPosition: "bottom",
+  captionDelay: 250,
+});
+
+export { galleryItemsArray };
